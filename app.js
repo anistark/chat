3a0f7b14533e2d0809b1450d8c9cf5653e155708
Kumar Anirudha <anirudhastark@yahoo.com>
2015-04-21 21:41:05 +0530

    initial design. chat page. home

--- app.js
+++ app.js
@@ -1,70 +1,54 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-// Database
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://localhost:27017/chat", {native_parser:true});
+/**
+ * Module dependencies.
+ */
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'),
+  routes = require('./routes'),
+  socket = require('./routes/socket.js');
 
-var app = express();
+var app = module.exports = express.createServer();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Hook Socket.io into Express
+var io = require('socket.io').listen(app);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Configuration
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-module.exports = app;
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/home', routes.home);
+app.get('/partials/:name', routes.partials);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Socket.io Communication
+
+io.sockets.on('connection', socket);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/*
+ * GET home page.
+ */
 
-router.get('/home', function(req, res, next){
-	res.render('home',{ title: 'Stark Chat'})
-});
+exports.index = function(req, res){
+  res.render('index');
+};
 
-module.exports = router;
+exports.partials = function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
+};
+
+exports.home = function(req, res){
+  res.render('home');
+};
